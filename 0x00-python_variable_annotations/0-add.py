a95ffd4700448be94e08549a587724a2ce36ef03
Sampul-CodeMine <studysampul@gmail.com>
2024-03-15 11:27:38 +0100

    Solution to Task 0 and Task 1

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This is a module to perform additional maths operation.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    This is a function to add two numbers (float)
+
+    Args:
+        a (float) - Floating number
+        b (float) - Floating number
+
+    Return:
+        (float) A floating number which is the sum of {a} and {b}
+    """
+    return (a + b)
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This is a module to perform string concatenation operation
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This is a function to concatenates two strings
+
+    Args:
+        str1 (str) - First string argument
+        str2 (str) - second string argument
+
+    Return:
+        (str) a concatenation of {str1} and {str2}
+    """
+    return f"{str1}{str2}"
