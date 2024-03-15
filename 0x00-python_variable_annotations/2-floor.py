08bd3a6f7c4107d47a2c0ab9fb186750fa9dd02c
Sampul-CodeMine <studysampul@gmail.com>
2024-03-15 11:41:51 +0100

    Solution to Task 2 to Task 4

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This is a module to perform math floor operation
+"""
+
+
+def floor(n: float) -> int:
+    """
+    This is a function to floor a float to integer value
+
+    Args:
+        n (float) - float number as argument
+
+    Return:
+        (int) integer value of {n}
+    """
+    return int(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This is a module to convert float to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This is a function to convert float to string
+
+    Args:
+        n (float) - float number as argument
+
+    Return:
+        (str) string annotation of {n}
+    """
+    return str(n)
