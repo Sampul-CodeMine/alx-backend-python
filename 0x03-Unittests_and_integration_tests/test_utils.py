4e302b31bf5fbd6f9772ac69caf48b23330b809f
Sampul-CodeMine <studysampul@gmail.com>
2024-04-06 12:35:59 +0100

    Task 0 Tested

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,33 @@
+#!/usr/bin.env python3
+"""
+This is a Unittest Module to Test the utils.py functionalities
+"""
+
+from unittest import (TestCase, mock)
+from unittest.mock import (Mock, patch)
+from parameterized import parameterized
+from typing import (Dict, Tuple, Union)
+from utils import (access_nested_map, get_json, memoize)
+
+
+class TestAccessNestedMap(TestCase):
+    """This is a class to test the access_nested_map function"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self,
+                               nested_map: dict,
+                               path: Tuple[str],
+                               expected: Union[Dict, int]) -> None:
+        """
+        This is a test method for the {access_nested_map} funtionality
+        Args:
+            nested_map (dict)
+            path (tuple of string)
+            expected (A union of {dict} and {int})
+        Returns:
+            None 
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
