eb5bae76b02b83ab3f08d9dee417b9c57b2d7584
Sampul-CodeMine <studysampul@gmail.com>
2024-04-06 13:52:34 +0100

    Task 4 Solution

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" This is a module to test Clients """
+from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+import utils
+import requests
+from unittest import TestCase
+from unittest.mock import (MagicMock, patch, Mock, PropertyMock, call)
+from parameterized import (parameterized, parameterized_class)
+
+
+class TestGithubOrgClient(TestCase):
+    """ Test that json can be gotten for request """
+    @parameterized.expand([
+        ("google", {"google": True}),
+        ("abc", {"abc": True})
+    ])
+    @patch('client.get_json')
+    def test_org(self, org: str, expected: dict, get_patch: MagicMock) -> None:
+        """This is a method to test the org method"""
+        get_patch.return_value = MagicMock(return_value=expected)
+        org_client = GithubOrgClient(org)
+        self.assertEqual(org_client.org(), expected)
+        get_patch.assert_called_once_with(
+            f"https://api.github.com/orgs/{org}"
+        )
